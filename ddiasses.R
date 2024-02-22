e9cda3fc8b648df3fc0dfa0b5270298850311cc9
Shah <yash_ankurbhai.shah@boehringer-ingelheim.com>
2024-02-22 10:20:14 +0100

    - add inhibitor table vals - some minor changes to excel file

--- ddiasses.R
+++ ddiasses.R
@@ -5,7 +5,8 @@ library(kableExtra)
 library(readxl) # for reading Excel files
 
 # Read the Excel file
-df2 <- read_excel("DDI template - Copy.xlsx", sheet = "Substrates for various CYPs")
+df_substrate <- read_excel("DDI template - Copy.xlsx", sheet = "Substrates for various CYPs")
+df_inhibitor <- read_excel("DDI template - Copy.xlsx", sheet = "Inhibitors for various CYPs")
 
 # Define UI
 ui <- fluidPage(
@@ -19,8 +20,9 @@ ui <- fluidPage(
                                                   "TDI"
                                      )))),
                column(3, selectInput("dropdown2", "Select substrates",
-                                     choices = unique(df2[["Enzyme full"]]))),
-               column(3, selectInput("dropdown3", "Select Inhibitors", choices = NULL)),
+                                     choices = unique(df_substrate[["Enzyme full"]]))),
+               column(3, selectInput("dropdown3", "Select Inhibitors",
+                                     choices = unique(df_inhibitor[["Enzyme full"]]))),
                column(3, selectInput("dropdown4", "[i]", choices = NULL),
                       actionButton("addrow", "Add row")) # Add row button
              ),
@@ -30,6 +32,9 @@ ui <- fluidPage(
     ),
     tabPanel("Tab 2",
              htmlOutput("table2") # Output for the second table
+    ),
+    tabPanel("Tab 3",
+             htmlOutput("table3") # Output for the second table
     )
   )
 )
@@ -37,12 +42,7 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output, session) {
   # Initialize the table data
-  table_data <- reactiveVal(data.frame(Compound.ID = character(),
-                                      substrateval = character() ,
-                                       figut = character(),
-                                      fgut = character(),
-                                      fm = character(),
-                                      fm.CYP = character()))
+  table_data <- reactiveVal(data.frame())
 
   # Render the table
   output$table <- renderUI({
@@ -51,7 +51,12 @@ server <- function(input, output, session) {
     # Create table with multi-level headers
     table_html <- kable(df, "html", row.names = FALSE) %>%
       kable_styling() %>%
-      add_header_above(c(" " = 2, "SUBSTRATE (VICTIM)" = 4), line = TRUE) %>%
+      add_header_above(c(" " = 1,
+                         "SUBSTRATE (VICTIM)" = 5,
+                         "REVERSIBLE INHIBITOR (PERPETRATOR)" = 12,
+                         "TDI" = 2
+                         ),
+                       line = TRUE) %>%
       column_spec(1, width = "50px") # Limit the width of the first column
 
     # Render the HTML code as a table
@@ -69,13 +74,31 @@ server <- function(input, output, session) {
   # Observe the submit button in the modal
   observeEvent(input$submit, {
     # Add the name to the first column of the table
-    selected_row <- df2[df2$`Enzyme full` == input$dropdown2, ]
+    selected_row_substr <- df_substrate[df_substrate$`Enzyme full` == input$dropdown2, ]
+    selected_row_inhibtr <- df_inhibitor[df_inhibitor$`Enzyme full` == input$dropdown3, ]
     new_row <- data.frame(Compound.ID = input$name,
-                          substrateval = selected_row$`Enzyme full`,
-                          figut = selected_row$fabs,
-                          fgut = selected_row$fgut,
-                          fm = selected_row$`fm (1-fe)`,
-                          fm.CYP = selected_row$fmCYP3A4)
+                          substrateval = selected_row_substr$`Enzyme full`,
+                          figut = selected_row_substr$fabs,
+                          fgut = selected_row_substr$fgut,
+                          fm = selected_row_substr$`fm (1-fe)`,
+                          fm.CYP = selected_row_substr$fmCYP3A4,
+                          inhibval = selected_row_inhibtr$`Enzyme full`,
+                          MW = selected_row_inhibtr$MW,
+                          Dose = selected_row_inhibtr$`Dose (mg)`,
+                          tau = selected_row_inhibtr$`τ (h)`,
+                          f_uplasma = selected_row_inhibtr$fu,
+                          Kiu = selected_row_inhibtr$`Ki,u (µM)`,
+                          ka_ = selected_row_inhibtr$`ka (min-1)`,
+                          f_abs = selected_row_inhibtr$fabs,
+                          f_gut = selected_row_inhibtr$fgut,
+                          "F" = selected_row_inhibtr$F,
+                          CLorCLF = selected_row_inhibtr$`CL/F (mL/min)`,
+                          elimrateK = "not sure",
+                          Kiu2 = ifelse(selected_row_inhibtr$`MBI/TDI` == 1,
+                                        selected_row_inhibtr$`Ki,u (µM)`,"NA"),
+                          Kinact = ifelse(selected_row_inhibtr$`MBI/TDI` == 1,
+                                          selected_row_inhibtr$`Kinact (min-1)`,"NA")
+                          )
     table_data(rbind(table_data(), new_row))
 
     # Close the modal
@@ -85,12 +108,23 @@ server <- function(input, output, session) {
   # Render the second table
   output$table2 <- renderUI({
     # Create table with kable
-    table_html2 <- kable(df2, "html") %>%
+    table_html2 <- kable(df_substrate, "html") %>%
       kable_styling()
 
     # Render the HTML code as a table
     HTML(table_html2)
   })
+
+  # Render the third table
+  output$table3 <- renderUI({
+    # Create table with kable
+    table_html3 <- kable(df_inhibitor, "html") %>%
+      kable_styling()
+
+    # Render the HTML code as a table
+    HTML(table_html3)
+  })
+
 }
 
 # Run the application
